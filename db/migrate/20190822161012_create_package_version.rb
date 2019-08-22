b9327d745ce21eb07ae3a422a22d45daddbd7478
Tony Hsu <tonyc.t.hsu@gmail.com>
2019-08-22 18:46:42 +0200

    Create package_versions table

---
+++ db/migrate/20190822161012_create_package_version.rb
@@ -0,0 +1,13 @@
+class CreatePackageVersion < ActiveRecord::Migration[5.1]
+  def change
+    create_table :package_versions do |t|
+      t.string      :name,    null: false
+      t.string      :version, null: false
+      t.string      :title
+      t.text        :description
+      t.timestamp   :published_at
+      t.timestamps null: false
+    end
+    add_index :package_versions, [:name, :version], unique: true
+  end
+end
