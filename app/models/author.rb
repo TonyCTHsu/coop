f1f20dbc4a9b9ca5b90c6db7c73619cfa4850c66
Tony Hsu <tonyc.t.hsu@gmail.com>
2019-08-25 22:48:50 +0200

    Remove unused method

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,12 +2,6 @@ class Author < ApplicationRecord
   has_many :authorships
   has_many :package_versions, through: :authorships
 
-  def update_info
-    description = CranService.description(self)
-
-    update(description.to_h)
-  end
-
   def path
     "#{name}_#{version}"
   end
